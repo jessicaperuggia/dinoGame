c04bde2e39138f6ef206c3448de9ed06ea7e2849
Jessica Peruggia <jessica.peruggia@hotmail.com>
2021-01-19 16:56:47 -0300

    finishing the functional part of the game

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
 let isJumping = false;
+let isGameOver = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if(event.keyCode === 32) {
@@ -11,14 +14,12 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
     isJumping = true;
 
     let upInterval = setInterval(() => {
         if(position >= 150) {
             clearInterval(upInterval);
-
+            
             // descendo
             let dowInterval = setInterval(() => {
                 if(position <= 0){
@@ -40,10 +41,30 @@ function jump() {
 function createCactus(){
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+   if(isGameOver) return;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else  if( cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // Game over
+            clearInterval(leftInterval);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
